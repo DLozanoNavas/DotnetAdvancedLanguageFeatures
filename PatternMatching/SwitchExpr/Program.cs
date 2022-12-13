857d20fb25195da84b668a314bc9255a8a2d5d05
Daniel Lozano <ceo@fenix-alliance.com>
2022-12-13 00:23:18 -0500

    Adds: Switch Expression Structural pattern matching exercise

--- PatternMatching/SwitchExpr/Program.cs
+++ PatternMatching/SwitchExpr/Program.cs
@@ -2,28 +2,51 @@
 // C# Pattern Matching using enhanced switch expressions
 
 // switch statements are very common in C#:
-string NumToString(int num) {
-    switch (num) {
-        case 1:
-            return "One";
-        case 2:
-            return "Two";
-        case 3:
-            return "Three";
-        default:
-            return "Unknown";
-    }
+string NumToString(int num)
+{
+    return num switch
+    {
+        1 => "One",
+        2 => "Two",
+        3 => "Three",
+        _ => "Unknown"
+    };
 }
 Console.WriteLine(NumToString(2));
 Console.WriteLine(NumToString(4));
 
 // TODO: Beginning with C# 7 and enhanced in C# 8, switch statements 
 // can now operate on just about any type
+string ShapeToString(object shape)
+{
+    switch (shape)
+    {
+        case Circle: 
+            return "Circle";
+        case Rectangle: return "Rectangle";
+        case Triangle: return "Triangle";
+        default:
+            return "Unknown";
+    }
+
+};
 
 
-// TODO: This can be made a little more concise with the switch expression, starting in C# 8
+Console.WriteLine(ShapeToString(new Circle { Radius = 10 }));
+Console.WriteLine(ShapeToString(new Triangle { Base = 10, Height = 5 }));
+Console.WriteLine(ShapeToString(new Rectangle { Length = 10, Width = 10 }));
 
-// Console.WriteLine(ShapeToString2(new Circle {Radius = 20}));
-// Console.WriteLine(ShapeToString2(new Circle {Radius = 10}));
-// Console.WriteLine(ShapeToString2(new Rectangle {Length = 10, Width = 10}));
-// Console.WriteLine(ShapeToString2(new Rectangle {Length = 5, Width = 10}));
+// This can be made a little more concise with the switch expression, starting in C# 8
+string ShapeToString2(object shape) => shape switch
+{
+    Circle {Radius:> 10} => "Big Circle",
+    Circle {Radius:<= 10} => "Small Circle",
+    Rectangle {Length: var l , Width: var w} when l == w => "Rectangle",
+    Triangle => "Triangle",
+    _ => "Unknown"
+};
+
+Console.WriteLine(ShapeToString2(new Circle { Radius = 20 }));
+Console.WriteLine(ShapeToString2(new Circle { Radius = 10 }));
+Console.WriteLine(ShapeToString2(new Rectangle { Length = 10, Width = 10 }));
+Console.WriteLine(ShapeToString2(new Rectangle { Length = 5, Width = 10 }));
