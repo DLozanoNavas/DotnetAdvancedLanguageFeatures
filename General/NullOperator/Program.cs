216dd2a0e7d44fe4590c0b878eab38128e147dd5
Daniel Lozano <ceo@fenix-alliance.com>
2022-12-12 22:59:54 -0500

    Adds: Null coalescense operator excercises.

--- General/NullOperator/Program.cs
+++ General/NullOperator/Program.cs
@@ -2,22 +2,44 @@
 // The null coalescing operator
 
 // A common scenario is to test a value for null and assign one if it is
-void OldSchoolLogString(string theString) {
-    if (theString == null) {
-        Console.WriteLine("No string given!");
-    }
-    else {
-        Console.WriteLine(theString);
+void OldSchoolLogString(string theString)
+{
+    switch (theString)
+    {
+        case null:
+            Console.WriteLine("No string given!");
+            break;
+        default:
+            Console.WriteLine(theString);
+            break;
     }
 }
 
 OldSchoolLogString("Test String");
 OldSchoolLogString(null);
 
-// TODO: the ?? operator returns the left-hand value if not null, or the right one if it is null
+// The ?? operator returns the left-hand value if not null, or the right one if it is null
+void LogString(string? theString)
+{
+    Console.WriteLine(theString ?? "No string given!");
+}
+LogString("Test String");
+LogString(null);
+
+// The ??= assigns a value if the left-hand value is null
+void AssignableLogString(string? theString)
+{
+    theString ??= "No string given!";
+    Console.WriteLine(theString);
+}
+AssignableLogString("Test String");
+AssignableLogString(null);
 
 
-// TODO: It's also allowable to throw an exception as part of the right-hand expression
-
-
-// TODO: The ??= assigns a value if the left-hand value is null
+// It's also allowable to throw an exception as part of the right-hand expression
+void ThrowableLogString(string? theString)
+{
+    Console.WriteLine(theString ?? throw new ArgumentNullException(nameof(theString)));
+}
+ThrowableLogString("Test String");
+ThrowableLogString(null);
